9ce5f317d422eee91fa3e7ff1f06158a269cb9dc
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-07-14 17:19:24 +0530

    Added Routes to pages

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import Login from "./pages/Login";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
+import ProductsList from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/products" element={<ProductsList />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Routes>
       {/* <NavLink className="text-red-700 bg-pink-300" to="/login">
         Login
